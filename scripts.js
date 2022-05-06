0d64950270e1ea06b3c969d3043fb2552b3f234d
Korey Nichols <koreynichols1@gmail.com>
2022-05-06 15:04:23 -0700

    finished the js for the component

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 let start = document.getElementById("start");
 let thankyou = document.getElementById("thankyou");
+let ratings = document.querySelectorAll(".ratingValue");
+let thankyouNote = document.querySelector(".thankyouSelection");
+let rating = 0;
 
 document.querySelector('input').addEventListener('click', test)
+
+ratings.forEach(function(e){
+    e.addEventListener('click', setRating);
+})
 
 function test() {
     start.classList.add('hidden')
     thankyou.classList.remove('hidden')
+    let text = thankyouNote.innerText.substring(0, 17) + rating + thankyouNote.innerText.substring(16);
+    thankyouNote.innerText = text
+}
+
+function setRating(e) {
+    ratings.forEach(r => r.classList.remove('selectedRating'))
+    this.classList.add('selectedRating');
+    rating = this.innerText
 }
